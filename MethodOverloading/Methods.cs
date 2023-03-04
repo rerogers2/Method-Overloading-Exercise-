2f9161cc9f0a720f12a08acc9addf5b1e891be7d
Robert Rogers <rerogers@gmail.com>
2023-03-04 14:40:21 -0600

    Method overload work complete

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public static decimal add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string add(int a, int b, Boolean c)
+        {
+            var sum = a + b;
+            if (c == true && sum != 1)
+            {
+                return $"{a + b} dollars";
+            }
+            else if (c == true && sum == 1)
+            {
+                return $"{a + b} dollar";
+            }
+            else
+            {
+                return $"The sum is {sum.ToString()} but it's not dollars";
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //checking work for integer
+            var a = 4;
+            var b = 12;
+            var intsum = Methods.add(a, b);
+            //checking work for decimal
+            var c = 3.45m;
+            var d = 2.76m;
+            var decsum = Methods.add(c, d);
+            //checking work for dollars
+            var dollarsum = Methods.add(a, b, true);
+
+            Console.WriteLine($"The sum of the integers: {intsum}");
+            Console.WriteLine($"The sum of the decimals: {decsum}");
+            Console.WriteLine(dollarsum);
         }
     }
 }
